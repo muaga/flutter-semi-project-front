a5ce3286100d8def815d60c406694561ed62d797
wlstjdgns <jin009471@naver.com>
2023-10-20 16:14:08 +0900

    브랜치 이동

--- lib/ui/pages/book_list_page/book_list_page.dart
+++ lib/ui/pages/book_list_page/book_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blog/ui/pages/book_list_page/widgets/book_list_body.dart';
+import 'package:flutter_blog/data/model/Book.dart';
+import 'package:flutter_blog/ui/pages/book_list_page/widgets/book_form.dart';
 
 class BookListPage extends StatelessWidget {
   const BookListPage({super.key});
@@ -7,7 +8,21 @@ class BookListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BookListBody(),
+      appBar: AppBar(),
+      body: GridView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          childAspectRatio: 1 / 1.5,
+        ),
+        itemCount: books.length,
+        itemBuilder: (context, index) {
+          return BookForm(books[index]);
+        },
+        // 더 많은 리스트 아이템을 추가할 수 있습니다.
+      ),
     );
   }
 }
--- lib/ui/pages/book_list_page/widgets/book_form.dart
+++ lib/ui/pages/book_list_page/widgets/book_form.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/data/model/Book.dart';
 
-class bookForm extends StatelessWidget {
-  // final String bookImg;
-  // final String bookTitle;
-  // final String bookWriter;
+class BookForm extends StatelessWidget {
+  final Book book;
 
-  const bookForm(
-      // this.bookImg,
-      // this.bookTitle,
-      // this.bookWriter,
-      );
+  const BookForm(this.book);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset('assets/book_images/book1.png'),
-        Text(
-          '트렌드코리아2024',
-          style: TextStyle(
-            fontSize: 16, // Adjust the font size as needed
-            fontWeight: FontWeight.bold, // You can change the font weight
-            color: Colors.black, // You can set the text color
+    return FittedBox(
+      child: Column(
+        children: [
+          Image.asset(book.bookImage, fit: BoxFit.cover),
+          Text(
+            book.title,
+            style: TextStyle(
+              fontSize: 16, // Adjust the font size as needed
+              fontWeight: FontWeight.bold, // You can change the font weight
+              color: Colors.black, // You can set the text color
+            ),
           ),
-        ),
-        Text(
-          '김난도 외',
-          style: TextStyle(
-            fontSize: 13, // Adjust the font size as needed
-            fontWeight: FontWeight.bold, // You can change the font weight
-            color: Colors.grey, // You can set the text color
+          Text(
+            book.author,
+            style: TextStyle(
+              fontSize: 13, // Adjust the font size as needed
+              fontWeight: FontWeight.bold, // You can change the font weight
+              color: Colors.grey, // You can set the text color
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
